4ba2f4166bd4d60c03276e761f0c44dbfa0557fc
Thiago José <thiago.jose32@outlook.com>
2023-06-15 18:36:07 -0300

    aula 15

--- Javascript/Aula 14/script.js
+++ Javascript/Aula 14/script.js
@@ -2,7 +2,7 @@ function defineTema() {
     var msg = window.document.getElementById("msg")
     var img = window.document.getElementById("imagem")
     var data = new Date()
-    var hora = 18//data.getHours()
+    var hora = data.getHours()
     if(hora > 5 && hora < 12) {
         msg.innerHTML = "Bom dia"
         img.src = "img/manha.jpg"

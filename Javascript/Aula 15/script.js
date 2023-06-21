377444a7554dd4e6b0e3efe25785e7f2f36ebf39
Thiago José <thiago.jose32@outlook.com>
2023-06-20 22:17:12 -0300

    Aula 15

--- Javascript/Aula 15/script.js
+++ Javascript/Aula 15/script.js
@@ -1,3 +1,21 @@
-function verificar() {
-    alert("Acertou")
+document.getElementById("submit").onclick = function() {
+    var radios = document.getElementsByName("sexo");
+    for(var i = 0; i < radios.length; i++) {
+        if(radios[i].checked) {
+            var dataNascimento = document.getElementById("dataNascimento").value
+            var dias = (Date.now().value - dataNascimento)
+            var hoje = Date.now() / (1000 * 3600 * 24)
+            var img = document.createElement("img")
+
+            img.setAttribute("id", "foto")
+            switch(radios[i].value) {
+                case "masculino":
+                        alert(`${dataNascimento} - ${hoje}`)
+                    break;
+                case "feminino":
+                    alert("Feminino");
+                break;
+            }
+        }
+    }
 }
